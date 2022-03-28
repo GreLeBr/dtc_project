9caec823dcf6cbe10779b0d0c3f145ed030d32bf
Gregoire Le Bras <gregoire.le.bras@gmail.com>
2022-03-28 09:23:24 +0000

    added queries

--- models/core/merged_all-traffic_car.sql
+++ models/core/merged_all-traffic_car.sql
@@ -5,7 +5,7 @@ b AS (SELECT * FROM {{ref('selected_car_all-traffic')  }} )
 
 
 SELECT a.t as date, a.label, a.mode as mode_of_transport, a.nb_usagers as multimod_users,
- b.libelle, b.q as car_debit, b.k as road_occupation, b.etat_trafic, b.geo_point_2d
+ b.libelle, b.q as car_debit, b.k as road_occupation, b.etat_trafic, b.geo_point_2d, b.t_1h
 FROM a
 JOIN b
 ON a.t = b.t_1h
--- models/core/merged_bike_car.sql
+++ models/core/merged_bike_car.sql
@@ -5,7 +5,8 @@ b AS (SELECT * FROM {{ref('selected_car_bike')  }} ),
 c AS (SELECT * FROM {{ref('selected_bike_car')  }} )
 
 SELECT 
- b.libelle, b.q as car_debit, b.k as road_occupation, b.etat_trafic,b.geo_point_2d, c.nom_compteur, c.sum_counts as bike_users
+ b.libelle, b.q as car_debit, b.k as road_occupation, b.etat_trafic,b.geo_point_2d,
+  c.nom_compteur, c.sum_counts as bike_users, c.date
 FROM b
 JOIN c
 
